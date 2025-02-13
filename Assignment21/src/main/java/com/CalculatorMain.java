d6fab9795fe02ab39841283bb88fcaa67a78c4cd
Aditya Londhe <aditya.londhe@wissen.com>
2025-02-13 18:05:32 +0530

    Test

--- Assignment21/src/main/java/com/CalculatorMain.java
+++ Assignment21/src/main/java/com/CalculatorMain.java
@@ -1,4 +1,15 @@
 package com;
+public class CalculatorMain
+{
+	public static void main(String args[])
+	{
+		System.out.println("Calculator Operations.....");
+		Calculator c1 = new Calculator();
+		c1.add(10,20);
+		c1.sub(10,20);
+		c1.mul(10,20);
+	}
+}
 class Calculator
 {
 	public int add(int a,int b)
@@ -23,15 +34,4 @@ class Calculator
 	}
 
 
-}
-public class CalculatorMain
-{
-	public static void main(String args[])
-	{
-		System.out.println("Calculator Operations.....");
-		Calculator c1 = new Calculator();
-		c1.add(10,20);
-		c1.sub(10,20);
-		c1.mul(10,20);
-	}
 }
