8811cc30c850cd657d8693b537b960f6b9e8f198
Aditya Londhe <aditya.londhe@wissen.com>
2025-02-13 16:03:59 +0530

    CalculatorMain class implementation

---
+++ Abc.java
@@ -0,0 +1,11 @@
+public class Abc
+{
+	public static void main(String args[])
+	{
+		System.out.println("<<-----********Welcome*********----->>");
+		System.out.println("From Java File Abc.java");
+		System.out.println("Compiling & Executing through Jenkins");
+		System.out.println("<<-----********Thank You*********----->>");
+
+	}
+}
