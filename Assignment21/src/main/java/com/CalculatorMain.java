eee1a66b297699591ae1829ef21a12db13aa8d1b
Aditya Londhe <aditya.londhe@wissen.com>
2025-02-13 17:57:48 +0530

    Test

--- Assignment21/src/main/java/com/CalculatorMain.java
+++ Assignment21/src/main/java/com/CalculatorMain.java
@@ -1,5 +1,29 @@
 package com;
+class Calculator
+{
+	public int add(int a,int b)
+	{
+		System.out.println("Addition : "+(a+b));
+		return a+b;
+	}
+	public int sub(int a,int b)
+	{
+		System.out.println("Subtraction : "+(a-b));
+		return a-b;
+	}
+	public int mul(int a,int b)
+	{
+		System.out.println("Multiplication : "+(a*b));
+		return a*b;
+	}
+	public int div(int a,int b)
+	{
+		System.out.println("Division : "+(a/b));
+		return (int)(a/b);
+	}
 
+
+}
 public class CalculatorMain
 {
 	public static void main(String args[])
